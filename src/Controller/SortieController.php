061ea2b9047e3854336a2beb2e035196e805462c
Kael Beneteau <kaL.beneteau2021@campus-eni.fr>
2021-05-25 11:29:00 +0200

    création de la page modifier sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\EtatRepository;
 use App\Repository\LieuRepository;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,7 +54,54 @@ class SortieController extends AbstractController
         return $this->render('sortie/creer.html.twig', [
             'sortieForm'=>$sortieForm->createView(),
             'user'=>$user,
-            'sortie'=>$sortie
+            'sortie'=>$sortie,
+            'modification'=>false
         ]);
+    }
+
+    /**
+     * @Route("/sortie/modifier/{id}", name="sortie_modifier")
+     */
+    public function modifier(int $id,Request $request,SortieRepository $sortieRepository, EtatRepository $etatRepository, EntityManagerInterface $entityManager) {
+
+        $sortie = $sortieRepository->find($id);
+
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        $sortieForm->handleRequest($request);
+
+        if($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            if ($request->get('submitAction') == 'enregistrer') {
+                $sortie->setEtat($etatRepository ->findOneBy(["libelle"=>"Créée"]));
+                $this->addFlash('warning',"Ta sortie est enregistrée ! Pense à la publier ;)");
+            }else {
+                $sortie->setEtat($etatRepository ->findOneBy(["libelle"=>"Ouverte"]));
+                $this->addFlash('success', "Ta sortie a bien été ajoutée !");
+            }
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('sortie_modifier',compact('id'));
+        }
+
+        return $this->render('sortie/creer.html.twig', [
+            'sortieForm'=>$sortieForm->createView(),
+            'sortie'=>$sortie,
+            'modification'=>true
+        ]) ;
+    }
+
+    /**
+     * @Route("/sortie/supprimer", name="sortie_supprimer")
+     */
+    public function supprimer(Request $request,SortieRepository $sortieRepository, EntityManagerInterface $entityManager) {
+
+        $sortie = $sortieRepository->findOneBy(['id' => $request->get('sortie_id')]);
+        $entityManager->remove($sortie);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('main_accueil') ;
+
     }
 }
