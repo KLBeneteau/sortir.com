2952ccef885b48eaf8ba3ea85ba58b8c7bc8f0ba
SylvainLG <sylvain.legoff2021@campus-eni.fr>
2021-05-25 13:46:59 +0200

    Sylvain : ajout méthode dans main controller et sortie repository

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Form\FiltreAccueilType;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,7 @@ class MainController extends AbstractController
     /**
      * @Route("/accueil", name="main_accueil")
      */
-    public function accueil(Request $request)
+    public function accueil(Request $request, SortieRepository $sortieRepository)
     {
 
         $sortiesForm = $this->createForm(FiltreAccueilType::class);
@@ -21,28 +22,12 @@ class MainController extends AbstractController
             'sortiesForm' => $sortiesForm->createView()
         ]);
 
+        $sortiesList = $sortieRepository->recherchesSorties();
+        return $this->render('main/accueil.html.twig', [
+           'sortiesList' => $sortiesList
+        ]);
 
 
 
-
-
-        //$donnees = new ();
-        //$rechercheDonnees = $this->createForm(::class, $donnees);
-        //return $this->render('main/accueil.html.twig',[
-         //  'rechercheDonneesForm' => $rechercheDonnees->createView()
-       // ]);
-
-
-
-
-       // $campusFiltre = new Campus();
-        //$campusList = $campusRepository->findAll($request->get('filtre'));
-        //$campusBisForm = $this->createForm(CampusBisType::Class,$campusFiltre);
-        //$campusBisForm->handleRequest($request);
-        //return $this->render('main/accueil.html.twig',[
-           // 'campusList' => $campusList,
-           // 'campusBisForm' => $campusBisForm->createView()
-
-        //]);
     }
 }
