044fbf0268b6fb773881826e2a592cd953f97738
Kael Beneteau <kaL.beneteau2021@campus-eni.fr>
2021-05-26 10:10:34 +0200

    fix login en créeant son compte

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Participant;
 use App\Form\CreerProfilType;
 use App\Repository\ParticipantRepository;
+use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class ProfilController extends AbstractController
 {
@@ -36,9 +38,13 @@ class ProfilController extends AbstractController
         Participant $participant = null,
         EntityManagerInterface $entityManager,
         UserPasswordEncoderInterface $passwordEncoder,
-        string $photoDir): Response
+        string $photoDir,
+        GuardAuthenticatorHandler $guardHandler,
+        AppAuthenticator $authenticator): Response
     {
+        $string = "modifié" ;
         if (!$participant){
+            $string = "créé" ;
             $participant = new Participant();
         }
         $profilForm = $this->createForm(CreerProfilType::class, $participant);
@@ -64,8 +70,14 @@ class ProfilController extends AbstractController
             $entityManager->persist($participant);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Votre profil a bien été créé');
-            return $this->redirectToRoute('profil_consulter', ['id' => $participant->getId()]);
+            $this->addFlash('success', 'Votre profil a bien été ' . $string);
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $participant,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
         }
         return $this->render('profil/creer-gerer.html.twig', [
             'profilForm' => $profilForm->createView(),
